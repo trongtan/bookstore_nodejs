63bab9e055239e9a22dbf461965e1f9273fb55f2
TanVu <tan.vu@eastagile.com>
2016-08-30 14:42:37 +0700

    [c] 971 - Add bootstrap css js [#129341971]

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -27,6 +27,8 @@ var jsFilesToInject = [
 
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
+  'js/dependencies/jquery.min.js',
+  'js/dependencies/bootstrap.min.js',
 
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
